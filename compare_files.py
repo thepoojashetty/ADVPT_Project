5db713949f902d7d3956d70555340b41ae8cfc33
zy69guqi <richard.angersbach@fau.de>
2023-12-11 12:08:42 +0100

    Refactor compare_files.py

--- compare_files.py
+++ compare_files.py
@@ -1,16 +1,5 @@
 import argparse
-
-# define input args
-parser = argparse.ArgumentParser(
-    prog='compare_files.py',
-    description='Compare two text files for equality')
-
-parser.add_argument('file1', type=str)
-parser.add_argument('file2', type=str)
-args = parser.parse_args()
-
-f1 = args.file1
-f2 = args.file2
+import os
 
 eps = 1e-6
 
@@ -21,7 +10,13 @@ def print_results(current, expected):
         print(f'{s1.strip()}\t{s2.strip()}')
 
 
-def check_results():
+def check_results(f1: str, f2: str):
+    if not os.path.isfile(f1):
+        raise ValueError(f'File {f1} does not exist.')
+
+    if not os.path.isfile(f2):
+        raise ValueError(f'File {f2} does not exist.')
+
     with open(f1, 'r') as curr, open(f2, 'r') as expect:
         # compare strings (potentially with floating point check)
         for s1, s2 in zip(curr, expect):
@@ -45,7 +40,19 @@ def check_results():
         return True
 
 
-if not check_results():
-    raise ValueError("File contents do not match")
+def main():
+    # define input args
+    parser = argparse.ArgumentParser(
+        prog='compare_files.py',
+        description='Compare two text files for equality')
 
-print("Success: File contents are identical!")
+    parser.add_argument('file1', type=str)
+    parser.add_argument('file2', type=str)
+    args = parser.parse_args()
+
+    if not check_results(args.file1, args.file2):
+        raise ValueError("File contents do not match")
+
+
+if __name__ == "__main__":
+    main()
