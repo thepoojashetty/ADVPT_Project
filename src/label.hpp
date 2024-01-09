c47ac33cd3be0741deb42c20a8c9642a312695ac
Usman <usman.lodhi@fau.de>
2024-01-09 12:04:21 +0100

    minor: working code but test fails

--- src/label.hpp
+++ src/label.hpp
@@ -13,65 +13,66 @@ private:
     size_t batch_size_;
     size_t number_of_labels_;
     std::vector<Eigen::MatrixXd> batches_;
+
 public:
     DatasetLabels(size_t batch_size);
     ~DatasetLabels();
-    void readLabelData(const std::string&);
-    // void writeLabelToFile(const std::string&,const size_t&);
+    void readLabelData(const std::string &);
+    void writeLabelToFile(const std::string &, const size_t &);
 };
 
 DatasetLabels::DatasetLabels(size_t batch_size)
 {
-    batch_size_=batch_size;
+    batch_size_ = batch_size;
 }
 
-DatasetLabels::~DatasetLabels(){}
+DatasetLabels::~DatasetLabels() {}
 
-//Reads file containing MNIST label data and stores it in batches
-//Makes use of Eigen library to store the data in a matrix
-void DatasetLabels::readLabelData(const std::string& input_filepath)
+// Reads file containing MNIST label data and stores it in batches
+// Makes use of Eigen library to store the data in a matrix
+void DatasetLabels::readLabelData(const std::string &input_filepath)
 {
-    std::ifstream input_file(input_filepath,std::ios::binary);
-    if(input_file.is_open())
+    std::ifstream input_file(input_filepath, std::ios::binary);
+    if (input_file.is_open())
     {
-        //read magic number
+        // Read magic number
         char bin_data[4];
         int magic_number;
-        input_file.read(bin_data,4);
-        std::reverse(bin_data,bin_data+4); //reverse magic number
+        input_file.read(bin_data, 4);
+        std::reverse(bin_data, bin_data + 4); // reverse magic number
         std::memcpy(&magic_number, bin_data, sizeof(int));
-        // std::cout<<"magic number: "<<magic_number<<std::endl;
 
-        //read number of labels
-        int number_of_labels=0;
-        input_file.read(bin_data,4);
-        std::reverse(bin_data,bin_data+4); //reverse magic number
+        // Read number of labels
+        int number_of_labels = 0;
+        input_file.read(bin_data, 4);
+        std::reverse(bin_data, bin_data + 4); // reverse magic number
         std::memcpy(&number_of_labels, bin_data, sizeof(int));
-        // std::cout<<"number of labels: "<<number_of_labels<<std::endl;
         number_of_labels_ = number_of_labels;
-    
-        //read label data
-        int label=0;
-        for(size_t i=0;i<number_of_labels_;i++)
+
+        // Read label data
+        int label = 0;
+        for (size_t i = 0; i < number_of_labels_; i++)
         {
-            input_file.read(bin_data,1);
-            std::memcpy(&label, bin_data, sizeof(int));
-            Eigen::MatrixXd label_matrix(1,10);
+            // Read an unsigned byte from the file
+            uint8_t byte;
+            input_file.read(reinterpret_cast<char *>(&byte), sizeof(byte));
+            // Convert byte to int
+            label = static_cast<int>(byte);
+            Eigen::MatrixXd label_matrix(1, 10);
             label_matrix.setZero();
-            label_matrix(0,label)=1;
+            // Set corresponding position to 1 (one-hot incoding)
+            label_matrix(0, label) = 1;
             batches_.push_back(label_matrix);
         }
     }
     else
     {
-        std::cout<<"Unable to open file"<<std::endl;
+        std::cout << "Unable to open file" << std::endl;
     }
 }
 
-/*
-// Untested
-//Writes label data to file
-void DatasetLabels::writeLabelToFile(const std::string& output_filepath,const size_t& index)
+// Writes label data to file
+void DatasetLabels::writeLabelToFile(const std::string &output_filepath, const size_t &index)
 {
     // Write in this format to be compatible with this format
     // 1
@@ -79,41 +80,29 @@ void DatasetLabels::writeLabelToFile(const std::string& output_filepath,const si
     // 0 if label is 0
     // 1 if label is 1
     // label 0, 1 should be written for respective digit entry
-    std::ofstream output_file(output_filepath,std::ios::binary);
-    if(output_file.is_open())
+    std::ofstream output_file(output_filepath, std::ios::binary);
+    if (output_file.is_open())
     {
-        //write magic number
-        int magic_number = 2049;
-        char bin_data[4];
-        std::memcpy(bin_data, &magic_number, sizeof(int));
-        std::reverse(bin_data,bin_data+4); //reverse magic number
-        output_file.write(bin_data,4);
+        
+        // Write the rank of the tensor to the file
+        output_file<<2<<"\n";
 
-        //write number of labels
-        int number_of_labels = 1;
-        std::memcpy(bin_data, &number_of_labels, sizeof(int));
-        std::reverse(bin_data,bin_data+4); //reverse magic number
-        output_file.write(bin_data,4);
+        // Write the shape of the tensor to the file
+        output_file<<10<<"\n";
 
-        //write label
-        Eigen::MatrixXd label_matrix = batches_[index];
-        int label = 0;
-        for(size_t i=0;i<10;i++)
+        // Write the tensor elements to the file
+        size_t batch_no = index / batch_size_;
+        size_t image_index = index % batch_size_;
+        size_t image_size = 10;
+        for(size_t i=0;i<image_size;++i)
         {
-            if(label_matrix(0,i)==1)
-            {
-                label = i;
-                break;
-            }
+            output_file<<batches_[batch_no](image_index,i)<<"\n";
         }
-        std::memcpy(bin_data, &label, sizeof(int));
-        output_file.write(bin_data,1);
+        output_file<<"\n";
+        output_file.close();
     }
     else
     {
-        std::cout<<"Unable to open file"<<std::endl;
+        std::cout << "Unable to open file" << std::endl;
     }
-
-    
 }
-*/
--- src/test_data_labels.cpp
+++ src/test_data_labels.cpp
@@ -2,16 +2,12 @@
 #include <string>
 #include "label.hpp"
 
-int main(){
-    // WIP: test code for label.hpp
-    // input file path should be "mnist-datasets/single-label.idx1-ubyte"
-    // output file path should be "mnist-temp-output/single-label.txt"
-
-    std::string input_filepath = "mnist-datasets/single-label.idx1-ubyte";
-    std::string output_filepath = "mnist-temp-output/single-label.txt";
-    // size_t index = std::stoi(0);
+int main(int argc,char* argv[]){
+    std::string input_filepath = argv[1];
+    std::string output_filepath = argv[2];
+    size_t index = std::stoi(argv[3]);
     DatasetLabels labels(5000); //batch size set to 5000
     labels.readLabelData(input_filepath);
-    // labels.writeImageToFile(output_filepath,index);
+    labels.writeLabelToFile(output_filepath,index);
     return 0;
 }
